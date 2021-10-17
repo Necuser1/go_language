7288037444cab78bcee5e71c5915bd176042a9b0
Neeraj-Nec <neeraj.srivastav@india.nec.com>
2021-10-17 14:14:19 +0000

    recover

--- error/panic/main.go
+++ error/panic/main.go
@@ -5,6 +5,11 @@ import (
 	"os"
 )
 
+func handlepanic() {
+    if a := recover(); a != nil {
+        fmt.Println("RECOVER", a)
+    }
+}
 func divideByZero() {
 	defer func() {
                 if r := recover() ; r != nil {
@@ -19,11 +24,7 @@ func divideByZero() {
 }
 
 func assersionError() {
-	defer func() {
-                if r := recover() ; r != nil {
-                        fmt.Printf("a panig is observed with value %v \n",r)
-                }
-        }()
+	defer handlepanic()
 	var str interface{}
 	fmt.Scan(&str)
 	fmt.Println("enter value 1 for executing error and 2 for normal flow")
