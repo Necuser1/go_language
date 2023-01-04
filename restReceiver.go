f514ebccb28afb435bb27ebc84d0e09c42421e56
Neeraj Srivastav <50608116+Neeraj-Nec@users.noreply.github.com>
2023-01-04 14:46:32 +0530

    Update restReceiver.go

--- restReceiver.go
+++ restReceiver.go
@@ -5,20 +5,23 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func Handler(c *gin.Context) {
 
-	data, err := ioutil.ReadAll(r.Body)
+	data, err := ioutil.ReadAll(c.Request.Body)
 	var unmarshalData interface{}
 	err = json.Unmarshal(data, &unmarshalData)
 	fmt.Println(unmarshalData)
 	fmt.Println(err)
-	w.WriteHeader(200)
+	c.JSON(http.StatusAccepted, "data data")
 
 }
 func main() {
-	http.HandleFunc("/ngsi-ld/listen", Handler)
-	http.ListenAndServe(":8080", nil)
+	app := gin.Default()
+	app.POST("/ngsi-ld/v1/entities/:id/attrs/:attribute", Handler)
+	fmt.Println("Server Started:")
+	app.Run(":8080")
 }
-
